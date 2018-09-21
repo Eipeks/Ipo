b3ce433cee4c7811690850e531eb42cdbc818f43
Eipeks <apexzdgr8est@gmail.com>
2018-09-21 23:06:08 +0100

    HomeViewModel tested

--- app/src/test/java/com/ayokunlepaul/ipo/ExampleUnitTest.kt
+++ app/src/test/java/com/ayokunlepaul/ipo/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package com.ayokunlepaul.ipo
 
+import com.ayokunlepaul.ipo.core.Placemark
+import com.ayokunlepaul.ipo.core.persistence.IpoPersistenceManager
+import com.ayokunlepaul.ipo.screens.MainActivityViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +16,25 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Mock
+    private lateinit var preferenceManager: IpoPersistenceManager
+
+    private lateinit var viewModel: MainActivityViewModel
+
+    @Before
+    fun setupMocks() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = MainActivityViewModel(preferenceManager)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun checkForSavedPlaceMark() {
+        Mockito.`when`(viewModel.getPlacemark()).thenReturn(Placemark("Lesserstraße 170, 22049 Hamburg", listOf(10.07526,53.59301),
+                "CE", "UNACCEPTABLE", 42, "UNACCEPTABLE", "HH-GO8522", "WME4513341K565439"))
+
+        val testPlacemark = Placemark("Lesserstraße 170, 22049 Hamburg", listOf(10.07526,53.59301),
+                "CE", "UNACCEPTABLE", 42, "UNACCEPTABLE", "HH-GO8522", "WME4513341K565439")
+        assertEquals(testPlacemark, viewModel.getPlacemark())
     }
 }
